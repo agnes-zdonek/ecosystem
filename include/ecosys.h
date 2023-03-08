258a1c73a320a1a268658fd9c2f5d77edf8f5f49
agnes-zdonek <agnieszka.zdonek@gmail.com>
2023-03-08 21:06:27 +0100

    finished comments in ecosys.h

--- include/ecosys.h
+++ include/ecosys.h
@@ -21,7 +21,7 @@ extern const int temps_repousse_herbe;
  * @param x first coordinate
  * @param y second coordinate
  * @param dir directions of move: 0 - nothing, 1 - up/left, -1 - down/right
- * @param energie energy
+ * @param energie energy > 0
  * @param suivant next animal in a linked list
  */
 typedef struct _animal {
@@ -33,92 +33,133 @@ typedef struct _animal {
 } Animal;
 
 /**
- * ... text ...
+ * @brief creates an animal
+ * @param x first coordinate
+ * @param y second coordinate
+ * @param energie energy
+ * @return created animal
  */
 Animal *creer_animal(int x, int y, float energie);
 
 /**
- * ... text ...
+ * @brief appends animal at the beggining of a list
+ * @param liste list
+ * @param animal animal
+ * @return pointer to the beggining of a list
  */
 Animal *ajouter_en_tete_animal(Animal *liste, Animal *animal);
 
 /**
- * ... text ...
+ * @brief recursive function, counts number of animals in a list
+ * @param la list of animals
+ * @return number of animals in a list
  */
 unsigned int compte_animal_rec(Animal *la);
 
 /**
- * ... text ...
+ * @brief iterative function, counts number of animals in a list
+ * @param la list of animals
+ * @return number of animals in a list
  */
 unsigned int compte_animal_it(Animal *la);
 
 /**
- * ... text ...
+ * @brief creates and appends animal at the beggining of a list
+ * @param x first coordinate
+ * @param y second coordinate
+ * @param energie energy
+ * @param liste_animal pointer to the list of animals
  */
 void ajouter_animal(int x, int y, float energie, Animal **liste_animal);
 
 /**
- * ... text ...
+ * @brief deletes an animal from a list
+ * @param liste pointer to the list of animals
+ * @param animal animal to delete
  */
 void enlever_animal(Animal **liste, Animal *animal);
 
 /**
- * ... text ...
+ * @brief frees list of animals and memory allocated to it
+ * @param liste liste of animals
+ * @return null
  */
 Animal* liberer_liste_animaux(Animal *liste);
 
 /**
- * ... text ...
+ * @brief prints a grind with predators and prey in it
+ * @param liste_predateur list of predators
+ * @param liste_proie list of prey
  */
 void afficher_ecosys(Animal *liste_predateur, Animal *liste_proie);
 
 /**
- * ... text ...
+ * @brief changes place of an animal according to what they have in dir[2]
+ * @param la animal
  */
 void bouger_animaux(Animal *la);
 
 /**
- * ... text ...
+ * @brief verfies if animal l is on x,y coordinates
+ * @param la animal
+ * @param x first coordinate
+ * @param y second coordinate
+ * @return pointer to the animal or null
  */
 Animal *animal_en_XY(Animal *l, int x, int y);
 
 /**
- * ... text ...
+ * @brief moves predators using bouger_animaux, manages their energy, if there is a prey on predator's case, predator eats it and has it's energy, reproduces predators
+ * @param liste_predateur pointer to the list of predators
+ * @param liste_proie pointer to the list of prey
  */
 void rafraichir_predateurs(Animal **liste_predateur, Animal **liste_proie);
 
 /**
- * ... text ...
+ * @brief moves prey using bouger_animaux, manages their energy, eats plant if it's in prey's case, reproduces prey
+ * @param liste_proie pointer to the list of prey
+ * @param monde the grid we place our animals/plants in
  */
 void rafraichir_proies(Animal **liste_proie, int monde[SIZE_X][SIZE_Y]) ;
 
 /**
- * ... text ...
+ * @brief regrows plants
+ * @param the grind we place our animals/plants in
  */
 void rafraichir_monde(int monde[SIZE_X][SIZE_Y]);
 
 /**
- * ... text ...
+ * @brief reproduces animals
+ * @param p_reproduce probability of reproduction
  */
 void reproduce(Animal **liste_animal, float p_reproduce);
 
 /**
- * ... text ...
+ * @brief clears terminal
  */
 void clear_screen();
 
 /**
- * ... text ...
+ * @brief writes one type of animals into a text file
+ * @param file file
+ * @param liste_animal list of animals
+ * @param type string indicating if those are predators/prey
  */
 void ecrire_animal(FILE *file, Animal *liste_anim, char *type);
 
 /**
- * ... text ...
+ * @brief writes list of animals into a text file
+ * @param nom_fichier name of an out file
+ * @param liste_predateur list of predators
+ * @param liste_poire list of prey
  */
 void ecrire_ecosys(const char *nom_fichier, Animal *liste_predateur, Animal *liste_proie);
 
 /**
- * ... text ...
+ * @brief reads list of animlas from a file to lists 
+ * @param nom_fichier name of an out file
+ * @param liste_predateur pointer to list of predators
+ * @param liste_poire pointer to list of prey
  */
 void lire_ecosys(const char *nom_fichier, Animal **liste_predateur, Animal **liste_proie);
 
